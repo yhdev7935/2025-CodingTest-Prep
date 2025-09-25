64b256a8361ee45b5c7b263d004d32a7c2a34393
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2025-09-26 02:42:31 +0900

    Add 15652 (0x0C - Backtracking)

---
+++ BarkingDog/0x0C-Backtracking/15652.cpp
@@ -0,0 +1,26 @@
+// BOJ - 15652 N과 M (4)
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int p[9] = {0, }, c[9] = {0, }, n, m;
+
+void f(int idx, int prev) {
+    if(idx == m + 1) {
+        for(int i = 1; i <= m; i++) cout << c[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i = prev; i <= n; i++) {
+        //if(p[i]) continue;
+        c[idx] = i; p[i] = 1;
+        f(idx + 1, i);
+        c[idx] = 0; p[i] = 0;
+    }
+}
+int main() {
+    cin >> n >> m;
+    f(1, 1);    
+}
